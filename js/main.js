ec45f4a9c6957d651cffc35074b4bc18da3e4db1
six6na <105402240+six6na@users.noreply.github.com>
2022-08-07 22:34:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 // main.js
 
 $(function(){
- 
-    $('#gnb .depth1 > li').hover(
-      
-      function(){
-        $(this)
-          .children('.depth2')
-          .addClass('on');
-      },
-      
-      function(){
-        $(this)
-        .children('.depth2')
-        .removeClass('on');
-      }
 
-    );
+    $('.tab .news-btn').click(function(){
+        // 텝 초기화
+        $('.tab_contents').removeClass('on');
+        // 표시
+        $('#news').addClass('on');
+        $('#noti').removeClass('on');
+        // 버튼
+        $('.tab .container-n .tab-btn li:first-child').addClass('on');
+        $('.tab .container-n .tab-btn li:last-child').removeClass('on');
+    });
+  
+    $('.tab .noti-btn').click(function(){
+        // 텝 초기화
+        $('.tab_contents').removeClass('on');
+        // 표시
+        $('#noti').addClass('on');
+        // 버튼
+        $('.tab .container-n .tab-btn li:last-child').addClass('on');
+        $('.tab .container-n .tab-btn li:first-child').removeClass('on');
+    });
+
 })
